2e916975e2b1dcf4d6baf112755be729fb17a3d1
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-11 12:08:07 +0200

    Connection to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/connectDB');
 dotenv.config();
 
 const app = express();
@@ -9,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 4500;
 
 // Connexion à la base de données
-// connectDB();
+connectDB();
 
 // Démarrer le serveur
 app.listen(PORT, () => {
