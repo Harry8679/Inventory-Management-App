89a61637b637758ef5f24a7d84739127e2bd16a6
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-12 23:19:59 +0200

    Login Controller Bug Fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -84,13 +84,15 @@ const login = asyncHandler(async(req, res) => {
     const token = generateToken(user._id);
 
     // Send HTTP-only cookie
-    res.cookie('token', token, {
-        path: '/',
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000 * 86400), // 1 day
-        sameSite: 'none',
-        secure: true,
-    });
+    if (passwordIsCorrect) {
+        res.cookie('token', token, {
+            path: '/',
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 86400), // 1 day
+            sameSite: 'none',
+            secure: true,
+        });
+    };
 
     if (user && passwordIsCorrect) {
         const { _id, name, email, photo, phone, bio } = user;
