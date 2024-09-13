9b8f8733e97be4dfc7d88db15d59543bff8ed9c1
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-13 13:12:56 +0200

    Create Login Status Controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -129,7 +129,19 @@ const getUser = asyncHandler(async (req, res) => {
 });
 
 const loginStatus = asyncHandler(async (req, res) => {
-    res.send('Login Status');
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json(false);
+    }
+
+    // Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (verified) {
+        return res.json(true);
+    }
+
+    return res.json(false);
 });
 
 module.exports = { home, register, login, logout, getUser, loginStatus };
