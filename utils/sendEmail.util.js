a6411b103371c6d3e109472fc7693b1e29d5f4b0
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-15 22:29:25 +0200

    Useless comment

--- utils/sendEmail.util.js
+++ utils/sendEmail.util.js
@@ -28,42 +28,4 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
     return transporter.sendMail(options);
 };
 
-
-// const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
-//     const transporter = nodemailer.createTransport({
-//         host: process.env.EMAIL_HOST,
-//         port: 587,
-//         auth: {
-//             user: process.env.EMAIL_USER,
-//             pass: process.env.EMAIL_PASS,
-//         },
-//         tls: {
-//             rejectUnauthorized: false
-//         }
-//     });
-
-//     // Option for sending email
-//     const options = {
-//         from: sent_from,
-//         to: send_to,
-//         replyTo: reply_to,
-//         subject: subject,
-//         html: message
-//     }
-
-//     // Send Email
-//     transporter.sendMail(options, function (ree, info) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(info);
-//         }
-//     });
-
-//     if (!user) {
-//         res.status(404);
-//         throw new Error('User does not exist');
-//     }
-// };
-
 module.exports = sendEmail;
