752249a49ebca6624749e85707ee2864d12e9cf3
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-16 10:59:02 +0200

    Reset Password Controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -271,13 +271,21 @@ const resetPassword = asyncHandler (async(req, res) => {
     // Find token in DB
     const userToken = await Token.findOne({
         token: hashedToken,
-        expiresAt: {$gt: Data.now()}
+        expiresAt: {$gt: Date.now()}
     });
 
     if (!userToken) {
         res.status(404);
         throw new Error('Invalid or Expired Token');
     }
+
+    // Find user
+    const user = await User.findOne({ _id: userToken.userId });
+    user.password = password;
+    await user.save();
+    res.status(200).json({
+        message: 'Password Reset Successful, Please Login'
+    });
 });
 
 
