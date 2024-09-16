8bc4b22d1b843f643d183d672b204d2c679cefb0
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-16 15:14:45 +0200

    Route protected

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,6 +5,6 @@ const { upload } = require('../utils/fileUpload.util');
 const router = express.Router();
 
 router.post('/', protect, upload.single('image'), createProduct);
-router.get('/', getProducts);
+router.get('/', protect, getProducts);
 
 module.exports = router;
